4c52277ae6942e78da01a31e050b031f35064182
BranRead <branrtread@gmail.com>
2023-10-09 15:09:20 -0300

    Bit of Bootstrap added to make it easier on the eyes

--- register.php
+++ register.php
@@ -58,29 +58,57 @@ function filter($input){
     <meta charset="utf-8">
     <title>Sign up</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script defer src="./js/script.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="/css/styles.css" rel="stylesheet">
 </head>
 <body>
 
-<div>
-    <h3>Register</h3>
-    <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <label for="username">Type in your username here</label><br>
-        <input id="username" name="username" type="text" value="<?php echo $username; ?>">
-        <span class="error"><?php echo $usernameErr; ?></span>
-        <br>
-        <label for="password">Type in your password here</label><br>
-        <input id="password" name="password" type="password">
-        <span class="error"><?php echo $passwordErr; ?></span><br>
-        <input type="checkbox" name="showPassword" id="showPass">
-        <label id="showPassLabel" for="showPass">Show password</label><br>
-        <label for="passwordConfirm">Please re-enter your password</label>
-        <br>
-        <input type="submit" value="Sign up">
-    </form>
-</div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-10 col-lg-6 register mx-auto p-5">
+                <h3 class="page-title">Register</h3>
+                <form class="d-flex flex-column" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                    <!--First name-->
+                    <label class="mt-2" for="fName">First name:</label>
+                    <input type="text" id="fName" name="fName">
+                    <!--Last name-->
+                    <label class="mt-2"  for="lName">Last name:</label>
+                    <input type="text" id="lName" name="lName">
+                    <!--Email-->
+                    <label class="mt-2"  for="email">E-Mail:</label>
+                    <input type="text" id="email" name="email">
 
 
+                    <!--Username-->
+                    <label class="mt-2"  for="username">Type in your username here</label>
+                    <input id="username" name="username" type="text" value="<?php echo $username; ?>">
+                    <span class="error"><?php echo $usernameErr; ?></span>
 
+                    <!--Password-->
+                    <label class="mt-2"  for="password">Type in your password here</label>
+                    <input id="password" name="password" type="password">
+                    <span class="error"><?php echo $passwordErr; ?></span>
+                    <div class="showPassword">
+                        <input type="checkbox" name="showPassword" id="showPass">
+                        <label id="showPassLabel" for="showPass">Show password</label>
+                    </div>
+
+
+                    <!--Password confirm-->
+                    <label class="mt-2"  for="passwordConfirm">Please re-enter your password</label>
+                    <input id="passwordConfirm" name="passwordConfirm">
+                    <div class="showPassword">
+                        <input type="checkbox" name="showPassword" id="showPassConfirm">
+                        <label id="showPassConfirmLabel" for="showPassConfirm">Show password</label>
+                    </div>
+
+                    <button type="submit" class="mt-2 submit btn btn-primary">Sign Up</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script defer src="./js/script.js"></script>
 </body>
 </html>
